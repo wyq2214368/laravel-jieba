1baa1bcc1a31972b4927bd14c487172339516bc3
wangyaqi03 <wangyaqi03@baidu.com>
2022-08-19 19:21:56 +0800

    singleton更换为instance

--- app/Providers/JiebaServiceProvider.php
+++ app/Providers/JiebaServiceProvider.php
@@ -27,23 +27,20 @@ class JiebaServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        ini_set('memory_limit', '1024M');
         //结巴分词
-        $this->app->singleton('Jieba', function () {
-            $jieba = new Jieba();
-            $jieba::init(array('dict'=>env('DICT_TYPE', 'normal'))); // 词典类型，小词典占用内存更小，可选择使用。 https://github.com/fukuball/jieba-php#%E5%85%B6%E4%BB%96%E8%A9%9E%E5%85%B8
-            return $jieba;
-        });
-        $this->app->singleton('JiebaAnalyse', function () {
-            $jiebaAnalyse = new JiebaAnalyse();
-            $jiebaAnalyse::init(array('dict'=>env('DICT_TYPE', 'normal')));
-            return $jiebaAnalyse;
-        });
-        $this->app->singleton('JiebaPosseg', function () {
-            $jiebaPosseg = new Posseg();
-            $jiebaPosseg::init();
-            return $jiebaPosseg;
-        });
+        $jieba = new Jieba();
+        $jieba::init(array('dict'=>env('DICT_TYPE', 'normal'))); // 词典类型，小词典占用内存更小，可选择使用。 https://github.com/fukuball/jieba-php#%E5%85%B6%E4%BB%96%E8%A9%9E%E5%85%B8
+
+        $jiebaAnalyse = new JiebaAnalyse();
+        $jiebaAnalyse::init(array('dict'=>env('DICT_TYPE', 'normal')));
+
+        $jiebaPosseg = new Posseg();
+        $jiebaPosseg::init();
+
+        // laravels singleton还是会实例化新对象，需要使用instance指定静态了常驻内存
+        $this->app->instance('Jieba', $jieba);
+        $this->app->instance('JiebaAnalyse', $jiebaAnalyse);
+        $this->app->instance('JiebaPosseg', $jiebaPosseg);
         Finalseg::init();
     }
 }
