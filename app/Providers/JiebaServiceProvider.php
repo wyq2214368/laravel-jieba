d8f59b92c80be5bb56e1a07686e6d956bfdc2787
neverMore <784915971@qq.com>
2022-11-23 15:09:51 +0800

    memory_limit

--- app/Providers/JiebaServiceProvider.php
+++ app/Providers/JiebaServiceProvider.php
@@ -27,6 +27,7 @@ class JiebaServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        ini_set('memory_limit', '1024M');
         //结巴分词
         $jieba = new Jieba();
         $jieba::init(array('dict'=>env('DICT_TYPE', 'normal'))); // 词典类型，小词典占用内存更小，可选择使用。 https://github.com/fukuball/jieba-php#%E5%85%B6%E4%BB%96%E8%A9%9E%E5%85%B8
